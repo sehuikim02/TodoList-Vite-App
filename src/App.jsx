26059693a4b14a13c28049202b6d5f98720eb9e6
sehuikim02 <lulla1613@gmail.com>
2025-09-03 00:00:06 +0900

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import { Component } from 'react';
+import { Component } from 'react'; 
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 import TodoListTemplate from './components/TodoListTemplate';
 
+const initialTodos = new Array(500).fill(0).map(
+(item, idx) => ({ id: idx, text: `일정 ${idx}`, checked: true })
+);
 class App extends Component {
   id = 3; // 이미 0,1,2 가 존재하므로 3으로 설정
-  // 상태변수를 포함하고 있는 state 객체
+  //상태변수를 포함하고 있는 state 객체
   state = {
     todo: '',
-    todos: [
-      { id: 0, text: '리액트 소개', checked: false },
-      { id: 1, text: '리액트 구조', checked: true },
-      { id: 2, text: '리액트 사용', checked: false }
-    ]
+    todos: initialTodos
+    // todos: [
+    //   { id: 0, text: '리액트 소개', checked: false },
+    //   { id: 1, text: '리액트 구조', checked: true },
+    //   { id: 2, text: '리액트 사용', checked: false }
+    // ]
   };
   //이벤트핸들러 함수 선언
   handleChange = (e) => {
     this.setState({
       todo: e.target.value // input field의 다음 바뀔 값
     });
-  }
+  }; //handleChange
 
-  // 새로 만든 new todo 를 정리
   handleCreate = () => {
     const { todo, todos } = this.state;
     const newTodo = {
@@ -34,32 +37,30 @@ class App extends Component {
       todos: [...todos, newTodo],
       todo: '', // input 초기화
     });
-  }
+  }; //handleCreate
 
   handleEnter = (e) => {
-    // 눌려진 키가 Enter Key 이면, handleCreate 호출
+    // 눌려진 키가 Enter Key 이면 handleCreate 호출
     if (e.keyCode === 13) {
       this.handleCreate();
     }
-  };
+  }; //handleEnter
 
   handleToggle = (id) => {
     const { todos } = this.state;
     this.setState({
       todos: todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo)
     });
-  };//handleToggle
+  }; //handleToggle
 
   handleRemove = (id) => {
     const { todos } = this.state;
     this.setState({
-      // boolean은 반드시 filter를 써야함, 같지 않은 애들만 필터링되고, 같은 애들만 필터링 되지 않으니까 삭제된것처럼 보임
       todos: todos.filter(todo => todo.id !== id)
     });
   }; //handleRemove
 
   render() {
-    // 자식끼리 대화 못해서, 부모쪽에서 다 선언해놓음
     const { todo, todos } = this.state;
     const { handleChange, handleCreate, handleEnter, handleToggle, handleRemove } = this;
 
@@ -69,13 +70,11 @@ class App extends Component {
           mytodo={todo}
           myEnter={handleEnter}
           myChange={handleChange}
-          myCreate={handleCreate}
-        />
+          myCreate={handleCreate} />
         }>
-        {/* TodoItemList.jsx에 값을 보냄*/}
-        <TodoItemList myTodos={todos}
-          myToggle={handleToggle}
-          myRemove={handleRemove}
+        <TodoItemList myTodos={todos} 
+            myToggle={handleToggle} 
+            myRemove={handleRemove}
         />
       </TodoListTemplate>
     );
--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -4,6 +4,14 @@ import TodoItem from './TodoItem';
 
 // app.jsx에서 전달한 배열들로 펼쳐서 TodoItem.jsx에 전달
 class TodoItemList extends Component {
+    /*
+        true 리턴 (myTodos 변수에 변동이 있는 경우) 이면 render 함수 다시 호출
+        false 리턴 (myTodos 변수에 변동이 없는 경우) 이면, render 함수 다시 호출 X (랜더링 생략)
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.myTodos !== nextProps.myTodos;
+    }
+
     render() {
         const { myTodos, myToggle, myRemove } = this.props;
         /* 
